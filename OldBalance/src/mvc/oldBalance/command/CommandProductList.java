af7836a10a20aacfee6f7d1c85c80ea4b8d787bd
seonghyun506 <seonghyun506@gmail.com>
2017-10-17 09:57:18 +0900

    123

--- OldBalance/src/mvc/oldBalance/command/CommandProductList.java
+++ OldBalance/src/mvc/oldBalance/command/CommandProductList.java
@@ -1,10 +1,12 @@
 package mvc.oldBalance.command;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import mvc.oldBalance.model.*;
-import mybatis.oldBalance.service.*;
+import mvc.oldBalance.model.ObProduct;
+import mybatis.oldBalance.service.ProductService;
 
 public class CommandProductList implements ProductCommand {
 	private String next;
@@ -14,7 +16,7 @@ public class CommandProductList implements ProductCommand {
 	}
 	
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-<<<<<<< HEAD
+
 		String cate = request.getParameter("cate");
 		
 		ProductService service = ProductService.getInstance();
@@ -22,9 +24,7 @@ public class CommandProductList implements ProductCommand {
 		List <ObProduct> productList = service.selectByMainCate(cate);	
 	    request.setAttribute("param", productList );
 	    
-=======
-		
->>>>>>> branch 'master' of https://github.com/seonghyun506/OldBalance.git
+
 		return next;
 	}
 }
