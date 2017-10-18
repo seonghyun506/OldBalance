0387b2a09e617fc30f17c4b72071db4fd5b16673
kosta <kosta@192.168.0.133>
2017-10-18 13:57:29 +0900

    test

--- OldBalance/WebContent/OldBalance/js/mymodify/mymodify.js
+++ OldBalance/WebContent/OldBalance/js/mymodify/mymodify.js
@@ -12,5 +12,5 @@ $(document).ready(function() {
 	$("input[name='postsearch']").click(function(){
 //		window.location.href = '/OldBalance_cpy/FindPost?cmd=findpost';
 		window.open("/OldBalance_cpy/OldBalance/oldBalanceFindPost.jsp","","weigh=500,height=400");
-	});
+	}); 
 		}); 
